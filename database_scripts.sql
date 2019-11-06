abdd72bb1e64e3a4c26d7181f22d676abe9ede74
Victor <99798304@ambev.com.br>
2019-11-06 18:54:44 -0300

    database modelling

--- database_scripts.sql
+++ database_scripts.sql
@@ -15,3 +15,38 @@ insert into items ("name") values
 ('Brinquedo');
 
 select * from items
+
+--#########################################
+
+
+create table "user"(
+	id serial primary key,
+	email varchar(256) not null,
+	name varchar(256) not null
+);
+
+create table list(
+	id serial primary key,
+	user_id int
+		references "user" on delete restrict,
+    list_name varchar(256)
+);
+
+create table gest_list(
+	id serial primary key,
+	user_id int
+		references "user" on delete restrict,
+    list_id int
+		references list on delete restrict
+);
+
+create table item(
+	id serial primary key,
+	guest_id int
+		references "user" on delete restrict,
+    list_id int
+		references list on delete restrict,
+	name varchar(256)
+);
+
+drop table items;
